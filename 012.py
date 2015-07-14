bdf025f4e66076481faa56a936abc3dacf044bcf
Tom Ravenscroft <toravenscroft@gmail.com>
2015-07-14 13:09:11 +0100

    Solution to problem 12

--- 012.py
+++ 012.py
@@ -1,22 +1,21 @@
 # Triangle number with 500 divisors
+import time
+
 def factorial(n):
     return n * (n + 1) / 2
 
 def nTriDivisors(n):
     c = 1 
-    x = 1
+    x = 0
     while c < n + 1:
         y = factorial(x) 
-        x = x + 1
-        if y % 2 == 0:
-            s = y/2
-            c += 1
-        for z in xrange(2, y, 2):
-            if y % z == 0 and y != s:
-                print y, c
+        for z in xrange(1, y, 1):
+            if y % z == 0:
                 c += 1
                 if c >= n:
                     return y
+        x = x + 1
         c = 1
 
-print nTriDivisors(500)
+start = time.time()
+print "The value is: " + str(nTriDivisors(100)) + " Which took: " + str(time.time() - start) + " seconds to calculate"
