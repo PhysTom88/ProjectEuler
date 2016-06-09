d8c6f93cb88913d151306a868f04c3bbeb474a8f
Tom Ravenscroft <toravenscroft@gmail.com>
2016-06-09 16:37:33 +0100

    formatting

--- VariousProblems/fib_seq.py
+++ VariousProblems/fib_seq.py
@@ -1,43 +1,43 @@
 def fibonacci_calculator(_value, option):
-	
-	def _get_nth_value(_value):
-		a, b = 0, 1
-		fib_values = [a]
-		while len(fib_values) <= _value:
-			a, b = b, b + a
-			fib_values.append(a)
 
-		return float(a)
+    def _get_nth_value(_value):
+        a, b = 0, 1
+        fib_values = [a]
+        while len(fib_values) <= _value:
+            a, b = b, b + a
+            fib_values.append(a)
 
-	def _get_sequence_less_than(_value):
-		a, b = 0, 1
-		fib_values = [a]
-		while a < _value:
-			a, b = b, b + a
-			if a < _value:
-				fib_values.append(a)
+        return float(a)
 
-		return fib_values
+    def _get_sequence_less_than(_value):
+        a, b = 0, 1
+        fib_values = [a]
+        while a < _value:
+            a, b = b, b + a
+            if a < _value:
+                fib_values.append(a)
 
-	def _get_sequence_length(_value):
-		a, b = 0, 1
-		fib_values = [a]
-		while len(fib_values) <= _value:
-			a, b = b, b + a
-			fib_values.append(a)
+        return fib_values
 
-		return fib_values
+    def _get_sequence_length(_value):
+        a, b = 0, 1
+        fib_values = [a]
+        while len(fib_values) <= _value:
+            a, b = b, b + a
+            fib_values.append(a)
 
-	options = {'nth-value': _get_nth_value(_value),
-	           'less-than': _get_sequence_less_than(_value),
-	           'sequence-of-length': _get_sequence_length(_value)}
+        return fib_values
 
-	try:
-		_return = options[option]
-	except KeyError, e:
-		_return = e
-	else:
-		return _return
+    options = {'nth-value': _get_nth_value(_value),
+               'less-than': _get_sequence_less_than(_value),
+               'sequence-of-length': _get_sequence_length(_value)}
+
+    try:
+        _return = options[option]
+    except KeyError, e:
+        _return = e
+    else:
+        return _return
 
 
 n = 100
