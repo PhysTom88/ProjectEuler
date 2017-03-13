c17392cd33313c3981b9d64bb990652a87cb288d
tom <tom.ravenscroft@havasww.com>
2017-03-13 10:48:16 +0000

    added hyphen to split words

--- VariousProblems/pig_latin.py
+++ VariousProblems/pig_latin.py
@@ -1,7 +1,7 @@
 def pig_latin(phrase):
     piggy = str()
     for word in phrase.split():
-        piggy += word[1:] + word[0] + "ay "
+        piggy += word[1:] + "-" + word[0] + "ay "
 
     return piggy
 
